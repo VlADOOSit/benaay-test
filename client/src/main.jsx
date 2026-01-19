100811c868e62abf53b6c94db49da5d940da8b88
unknown <vlad.kharkovskiy10@gmail.com>
2026-01-19 15:10:24 +0200

    refactor code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './reset.css';
-import './tokens.css';
 import './global.css';
 import App from './App.jsx';
 
